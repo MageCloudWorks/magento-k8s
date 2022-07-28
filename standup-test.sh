98109e551fa43dfdb457edb4ef40a0515f1397bf
MageCloudWorks <laurence@magecloudworks.co.uk>
2022-07-28 10:32:01 +0100

    Create standup-test.sh

---
+++ standup-test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Check all pods are running
+PODSTATUS=$(kubectl get pods -n magento | grep -v "Running")
+[ $(echo $PODSTATUS | wc -l) -gt 1 ] && echo "Error not all pods running\n $PODSTATUS"
+
+# Basic test that varnish is responding
+K8IP=$(kubectl get nodes -o jsonpath={.items[0].status.addresses[?\(@.type==\"InternalIP\"\)].address})
+curl  http://$K8IP:30081/health_check.php
+curl  http://$K8IP:30081/ | grep "CMS homepage content goes here."
