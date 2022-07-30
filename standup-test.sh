bf5ce864dec72eb843739bec8818d65720714018
Laurence Baldwin <laurence@carbon.lancia.za.org>
2022-07-30 12:17:16 +0100

    fixed stand up test

--- standup-test.sh
+++ standup-test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 # Check all pods are running
 PODSTATUS=$(kubectl get pods -n magento | grep -v "Running")
 [ $(echo $PODSTATUS | wc -l) -gt 1 ] && echo "Error not all pods running\n $PODSTATUS"
@@ -6,4 +6,4 @@ PODSTATUS=$(kubectl get pods -n magento | grep -v "Running")
 # Basic test that varnish is responding
 K8IP=$(kubectl get nodes -o jsonpath={.items[0].status.addresses[?\(@.type==\"InternalIP\"\)].address})
 curl  http://$K8IP:30081/health_check.php
-curl  http://$K8IP:30081/ | grep "CMS homepage content goes here."
+curl  http://$K8IP:30081/ | grep "Magento"
