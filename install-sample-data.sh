b6ec06eebfe74f54176a39e4510d751103eb311f
Laurence Baldwin <laurence@carbon.lancia.za.org>
2022-08-04 12:53:38 +0100

    unit test debuging, pin libxml

--- install-sample-data.sh
+++ install-sample-data.sh
@@ -7,6 +7,7 @@ source env.sh
 echo install sample data
 kubectl exec -n magento deployment/apache -it -- sudo -u www-data composer -d/var/www/html/ config repositories.magento composer https://repo.magento.com
 kubectl exec -n magento deployment/apache -it -- sudo -u www-data composer -d/var/www/html/ config http-basic.repo.magento.com $REPOMAGENTOCOM_USER $REPOMAGENTOCOM_PASS
+kubectl exec -n magento deployment/apache -it -- sudo -u www-data composer -d/var/www/html/ update
 kubectl exec -n magento deployment/apache -it -- sudo -u www-data php /var/www/html/bin/magento sampledata:deploy
 kubectl exec -n magento deployment/apache -it -- sudo -u www-data php /var/www/html/bin/magento setup:upgrade
 
