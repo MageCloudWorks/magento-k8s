375298daeede22cab69e881c1476b1c2f8b3816b
Laurence Baldwin <laurence@carbon.lancia.za.org>
2022-08-09 10:13:30 +0100

    remove -t as Jenkins doesnt provide terminal, change elasticsearch version

--- check-versions.sh
+++ check-versions.sh
@@ -9,14 +9,14 @@ REDIS_REQ=6.2
 VARNISH_REQ=7.0
 APACHE_REQ=2.4
 
-COMPOSER_VER=$(kubectl exec -n magento deployment/apache -it -- sh -c 'sudo -u www-data composer -V | cut -f3 -d" " | cut -c1-3')
-ELASTICSEARCH_VER=$(kubectl exec -n magento deployment/apache -it -- curl -s 'http://elasticsearch:9200' | grep "number" | awk '{print $3}' | cut -c2-5)
-MARIADB_VER=$(kubectl exec -n magento deployment/mariadb -it -- mysqld -V | awk '{print $3}' | cut -c1-4)
-PHP_VER=$(kubectl exec -n magento deployment/apache -it -- php -v | head -1 | awk '{print $2}' | cut -c1-3)
+COMPOSER_VER=$(kubectl exec -n magento deployment/apache -i -- sh -c 'sudo -u www-data composer -V | cut -f3 -d" " | cut -c1-3')
+ELASTICSEARCH_VER=$(kubectl exec -n magento deployment/apache -i -- curl -s 'http://elasticsearch:9200' | grep "number" | awk '{print $3}' | cut -c2-5)
+MARIADB_VER=$(kubectl exec -n magento deployment/mariadb -i -- mysqld -V | awk '{print $3}' | cut -c1-4)
+PHP_VER=$(kubectl exec -n magento deployment/apache -i -- php -v | head -1 | awk '{print $2}' | cut -c1-3)
 #RABBITMQ_VER=
-REDIS_VER=$(kubectl exec -n magento deployment/redis-session -it -- redis-server -v |  awk '{print $3}' | cut -c3-5)
-VARNISH_VER=$(kubectl exec -n magento deployment/varnish -it -- varnishd -V 2>&1  | head -1 |  awk '{print $2}' | cut -c10-12)
-APACHE_VER=$(kubectl exec -n magento deployment/apache -it -- apache2 -v | awk  '{print $3}' | cut -c8-10 | head -1)
+REDIS_VER=$(kubectl exec -n magento deployment/redis-session -i -- redis-server -v |  awk '{print $3}' | cut -c3-5)
+VARNISH_VER=$(kubectl exec -n magento deployment/varnish -i -- varnishd -V 2>&1  | head -1 |  awk '{print $2}' | cut -c10-12)
+APACHE_VER=$(kubectl exec -n magento deployment/apache -i -- apache2 -v | awk  '{print $3}' | cut -c8-10 | head -1)
 
 RETURN=0
 
